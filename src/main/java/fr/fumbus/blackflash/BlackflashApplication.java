e2b8f4cdc1a2d9b3b251c7e7aa4605481cd7de9c
Jérémy Laurent <poulpy2k@gmail.com>
2026-03-24 22:24:35 +0100

    Initial commit 🚀

---
+++ src/main/java/fr/fumbus/blackflash/BlackflashApplication.java
@@ -0,0 +1,13 @@
+package fr.fumbus.blackflash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackflashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackflashApplication.class, args);
+    }
+
+}
