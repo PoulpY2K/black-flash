3955672997440a45350875eb6fa12106afa45e01
Jérémy Laurent <poulpy2k@gmail.com>
2026-03-30 22:39:08 +0200

    feat: enhance SlashCommandListener with audio player integration and initialization

--- src/main/java/fr/fumbus/blackflash/discord/jda/slash/SlashCommandListener.java
+++ src/main/java/fr/fumbus/blackflash/discord/jda/slash/SlashCommandListener.java
@@ -1,10 +1,15 @@
 package fr.fumbus.blackflash.discord.jda.slash;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import fr.fumbus.blackflash.discord.lavaplayer.GuildMusicManager;
 import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * @author Jérémy Laurent <poulpy2k>
@@ -12,13 +17,16 @@ import org.springframework.stereotype.Component;
  */
 
 @Log4j2
-@Component
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class SlashCommandListener extends ListenerAdapter {
     // TODO: add all music logic here, and split into separate services if needed
+    private AudioPlayerManager playerManager;
+    private Map<Long, GuildMusicManager> musicManagers;
 
     @PostConstruct
     public void init() {
+        this.playerManager = new DefaultAudioPlayerManager();
+        AudioSourceManagers.registerRemoteSources(playerManager);
         log.info("SlashCommandListener initialized and ready to handle slash commands.");
     }
 }
